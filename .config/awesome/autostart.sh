e622960bc55b13680af51e1c6796e1716f4b1eaf
Lohit244 <lohit244@gmail.com>
2022-05-18 11:36:49 +0530

    more updates

--- .config/awesome/autostart.sh
+++ .config/awesome/autostart.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash 
 
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/kdeconnectd &
 picom --config ~/.config/picom/picom.conf --experimental-backends &
 nm-applet &
 blueman-applet &
 /home/lohit244/mouse.sh &
+kdeconnect-indicator &
 dunst &
 autorandr -c &
 touchegg & disown
---
+++ mouseTransMat.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xinput --set-prop "pointer:Logitech G304" "Coordinate Transformation Matrix" 0.7, 0, 0, 0, 0.7, 0, 0, 0, 1
